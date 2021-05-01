915c7f6fc15b8fa99902188a5dcc2e71a27d8e57
gourab98 <gourabsahag@gmail.com>
2021-05-01 22:31:51 +0600

    CardProgramming

---
+++ Dart Programming/DeckCardOOP.dart
@@ -0,0 +1,28 @@
+void main(){
+  
+  var Deck = new Deck();
+}
+
+class Deck{  
+  
+  List <Card> cards;
+  
+  Deck(){
+      var ranks= ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King'];
+    var suits=['Diamonds','Hearts','Clubs','Spades'];
+    
+    for(var suit in suits){
+      for(var rank in ranks){
+        var card = new Card(rank,suit);  
+        cards.add(card);
+      }
+    }
+  }
+}
+
+class Card{
+  
+  String suit;
+  String rank;
+  Card(this.rank,this.suit);
+}
