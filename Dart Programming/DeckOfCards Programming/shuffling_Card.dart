71c29e1534d640019c24672ff2c2615e2f2c0c48
gourab98 <gourabsahag@gmail.com>
2021-05-06 21:06:15 +0600

    Shuffle method of the list is added

---
+++ Dart Programming/DeckOfCards Programming/shuffling_Card.dart
@@ -0,0 +1,46 @@
+void main(){
+    var deck= new Deck(); 
+    deck.shuffle();
+    print(deck);
+}
+
+class Deck
+{
+  List <Card> cards = [];
+  
+  Deck(){
+    var ranks = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King'];
+    var suits = ['Diamonds','Hearts','Clubs','Spades'];
+    
+    for (var suit in suits){
+      for (var rank in ranks){
+        
+        var card = new Card(rank, suit);
+        cards.add(card);
+        
+      }
+    }
+  }
+
+  toString(){
+    return cards.toString();
+  }
+  
+  shuffle(){
+    cards.shuffle();
+  }
+  
+}
+
+class Card{
+  String suit;
+  String rank;
+  
+  Card(this.rank , this.suit);
+  
+      toString(){
+      return '$rank of $suit';
+    }
+  
+}
+
