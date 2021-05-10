1438c28f15bee654953dfcd52f7fe6f0208a7aa4
gourab98 <gourabsahag@gmail.com>
2021-05-10 11:18:16 +0600

    external widget package add

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,1 +1,25 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
 
+// Create a class that will be our custom widget. This class
+// must extend the 'StatelessWidget' base class
+
+class App extends StatelessWidget {
+// Must define a 'build' method that returns the widgets
+// that *this* widget will show
+
+  Widget build(context) {
+    return MaterialApp(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("More photos!");
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ));
+  }
+}
