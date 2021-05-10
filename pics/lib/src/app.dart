530c45825170021c2ae8f5a676fe1d9124320575
gourab98 <gourabsahag@gmail.com>
2021-05-10 11:44:46 +0600

    resource photos added

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,13 +1,8 @@
-// Import flutter helper library
 import 'package:flutter/material.dart';
 
-// Create a class that will be our custom widget. This class
-// must extend the 'StatelessWidget' base class
+class App extends StatefulWidget {}
 
-class App extends StatelessWidget {
-// Must define a 'build' method that returns the widgets
-// that *this* widget will show
-
+class AppState extends State<App> {
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
